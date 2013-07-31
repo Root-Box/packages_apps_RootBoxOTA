a2f3db3553ad788a3244d9bf6b4426510e4a2d55
MNT <mnt@rootbox.ca>
2013-07-31 04:42:55 +0300

    Changelog page added

--- src/com/rootbox/rootboxota/fragments/ChangelogFragment.java
+++ src/com/rootbox/rootboxota/fragments/ChangelogFragment.java
@@ -32,7 +32,7 @@ import com.rootbox.rootboxota.R;
 public class ChangelogFragment extends Fragment {
 
     private static final String CHANGELOG_URL
-            = "https://plus.google.com/app/basic/115833651542488654391/posts";
+            = "http://api.rootbox.ca/changelog";
     private ProgressBar mProgressBar;
 
     @Override
